e5e04edc1676aae3b2cab6308a1ac317502cc8b9
Denis Perdomo <joanp307@gmail.com>
2022-04-13 20:33:53 -0400

    Now able to change the sq color buffer

---
+++ src/lib.rs
@@ -0,0 +1,137 @@
+use wgpu::*;
+
+use winit::event_loop::ControlFlow;
+use winit::window::Window;
+
+use std::rc::Rc;
+
+// Conveniency function to not type that conversion
+// all the time
+pub fn color_to_arr(color: Color) -> [f32; 4] {
+    [
+        color.r as f32,
+        color.g as f32,
+        color.b as f32,
+        color.a as f32,
+    ]
+}
+
+// Why Reference Counters?
+// I need shared ownership because
+// 1. Those three fields will be flying around
+// multiple structs, and
+// 2. A normal reference will not do
+// apparently because of behind-the-scenes
+// stuff Rust does with closures, but with
+// shared ownership Rust can do everything
+// it wants but the memory stays on the heap
+// and it gets cleaned up at the end of the last
+// owner's lifetime
+pub struct WgpuState {
+    pub surface: Rc<Surface>,
+    pub device: Rc<Device>,
+    pub queue: Rc<Queue>,
+    pub config: SurfaceConfiguration,
+}
+
+impl WgpuState {
+    pub async fn new(window: &Window) -> WgpuState {
+        let instance = Instance::new(Backends::all());
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&RequestAdapterOptions {
+                power_preference: PowerPreference::default(),
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &DeviceDescriptor {
+                    label: None,
+                    features: Features::empty(),
+                    limits: Limits::default(),
+                },
+                None,
+            )
+            .await
+            .unwrap();
+
+        let config = SurfaceConfiguration {
+            usage: TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_preferred_format(&adapter).unwrap(),
+            width: window.inner_size().width,
+            height: window.inner_size().height,
+            present_mode: PresentMode::Fifo,
+        };
+        surface.configure(&device, &config);
+
+        Self {
+            surface: Rc::new(surface),
+            device: Rc::new(device),
+            queue: Rc::new(queue),
+            config,
+        }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    pub fn clear_screen(
+        &self,
+        color: Color,
+        surface_texture: &SurfaceTexture,
+    ) -> Result<(), SurfaceError> {
+        // let surface_texture = self.surface.get_current_texture()?;
+
+        let view = surface_texture
+            .texture
+            .create_view(&TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&CommandEncoderDescriptor {
+                label: Some("render_encoder"),
+            });
+
+        let render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
+            label: Some("render_pass"),
+            color_attachments: &[RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: Operations {
+                    load: LoadOp::Clear(color),
+                    store: true,
+                },
+            }],
+            depth_stencil_attachment: None,
+        });
+
+        drop(render_pass);
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+
+        Ok(())
+    }
+
+    pub fn handle_render_result(
+        &mut self,
+        result: Result<(), SurfaceError>,
+        control_flow: &mut ControlFlow,
+        window: &Window,
+    ) {
+        match result {
+            Ok(_) => {}
+            Err(SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+            Err(SurfaceError::Lost) => self.resize(window.inner_size()),
+            Err(error) => eprintln!("{:?}", error),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod grid_drawer;
+use grid_drawer::*;
+
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -6,39 +9,53 @@ use winit::{
 
 use wgpu::*;
 
+use gol::*;
+
 fn main() {
     env_logger::init();
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Conway's Game of Life")
+        .with_min_inner_size(winit::dpi::LogicalSize {
+            width: 800u32,
+            height: 450u32,
+        })
+        .build(&event_loop)
+        .unwrap();
 
-    window.set_title("Conway's Game of Life");
-    window.set_min_inner_size(Some(winit::dpi::LogicalSize {
-        width: 800,
-        height: 450,
-    }));
+    let mut wgpu_state = pollster::block_on(WgpuState::new(&window));
 
-    let (surface, device, queue, mut configuration) = pollster::block_on(init_wgpu(&window));
+    let grid = GridDrawer::new(&wgpu_state);
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::RedrawRequested(window_id) if window_id == window.id() => {
-            match clear_screen(
-                Color {
-                    r: 0.01,
-                    g: 0.01,
-                    b: 0.1,
-                    a: 1.0,
-                },
-                &surface,
-                &device,
-                &queue,
-            ) {
-                Ok(_) => {}
-                Err(wgpu::SurfaceError::Lost) => {
-                    resize(&mut configuration, &surface, &device, window.inner_size())
+            match wgpu_state.surface.get_current_texture() {
+                Ok(surface_texture) => {
+                    let mut results = Vec::new();
+
+                    results.push(wgpu_state.clear_screen(
+                        Color {
+                            r: 0.1,
+                            g: 0.1,
+                            b: 0.1,
+                            a: 1.0,
+                        },
+                        &surface_texture,
+                    ));
+
+                    results.push(grid.draw(&surface_texture));
+
+                    if !results.iter().any(|result| result.is_err()) {
+                        surface_texture.present()
+                    } else {
+                        for result in results.into_iter() {
+                            wgpu_state.handle_render_result(result, control_flow, &window)
+                        }
+                    }
                 }
-                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                Err(error) => eprintln!("{:?}", error),
-            }
+
+                Err(error) => wgpu_state.handle_render_result(Err(error), control_flow, &window),
+            };
         }
 
         Event::MainEventsCleared => {
@@ -49,104 +66,21 @@ fn main() {
             ref event,
             window_id,
         } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            WindowEvent::CloseRequested => {
+                *control_flow = ControlFlow::Exit;
+            }
+
             WindowEvent::Resized(physical_size) => {
-                resize(&mut configuration, &surface, &device, *physical_size);
+                wgpu_state.resize(*physical_size);
             }
+
             WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                resize(&mut configuration, &surface, &device, **new_inner_size);
+                wgpu_state.resize(**new_inner_size);
             }
+
             _ => {}
         },
 
         _ => {}
     });
-}
-
-async fn init_wgpu(
-    window: &winit::window::Window,
-) -> (Surface, Device, Queue, SurfaceConfiguration) {
-    let instance = Instance::new(Backends::all());
-    let surface = unsafe { instance.create_surface(window) };
-    let adapter = instance
-        .request_adapter(&RequestAdapterOptions {
-            power_preference: PowerPreference::default(),
-            force_fallback_adapter: false,
-            compatible_surface: Some(&surface),
-        })
-        .await
-        .unwrap();
-
-    let (device, queue) = adapter
-        .request_device(
-            &DeviceDescriptor {
-                label: None,
-                features: Features::empty(),
-                limits: Limits::default(),
-            },
-            None,
-        )
-        .await
-        .unwrap();
-
-    let config = SurfaceConfiguration {
-        usage: TextureUsages::RENDER_ATTACHMENT,
-        format: surface.get_preferred_format(&adapter).unwrap(),
-        width: window.inner_size().width,
-        height: window.inner_size().height,
-        present_mode: PresentMode::Fifo,
-    };
-    surface.configure(&device, &config);
-
-    (surface, device, queue, config)
-}
-
-fn resize(
-    config: &mut SurfaceConfiguration,
-    surface: &Surface,
-    device: &Device,
-    new_size: winit::dpi::PhysicalSize<u32>,
-) {
-    if new_size.width > 0 && new_size.height > 0 {
-        config.width = new_size.width;
-        config.height = new_size.height;
-        surface.configure(device, config);
-    }
-}
-
-fn clear_screen(
-    color: Color,
-    surface: &Surface,
-    device: &Device,
-    queue: &Queue,
-) -> Result<(), SurfaceError> {
-    let output = surface.get_current_texture()?;
-
-    let view = output
-        .texture
-        .create_view(&TextureViewDescriptor::default());
-
-    let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor {
-        label: Some("render_encoder"),
-    });
-
-    let render_pass = encoder.begin_render_pass(&RenderPassDescriptor {
-        label: Some("render_pass"),
-        color_attachments: &[RenderPassColorAttachment {
-            view: &view,
-            resolve_target: None,
-            ops: Operations {
-                load: LoadOp::Clear(color),
-                store: true,
-            },
-        }],
-        depth_stencil_attachment: None,
-    });
-
-    drop(render_pass);
-
-    queue.submit(std::iter::once(encoder.finish()));
-    output.present();
-
-    Ok(())
 }
